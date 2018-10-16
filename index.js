d33640b6495e630773b851905076e24c6290433b
ymitz <ymitz32@gmail.com>
2018-10-16 15:10:31 +0900

    bugfix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // sort
 
-let random_list = [6,4,7,5,1,111,34]
+let random_list = [6,4,7,5,1010,111,34]
 
 let result = []
 //result.push(random_list.sort((a,b)=>a-b))
@@ -8,21 +8,18 @@ let result = []
 let sortedfunc = (list) => list.reduce(
     (prev, current, index) => {
         if (index === 0) {
-            console.log('index 0')
             prev.push(current)
             return prev
         }
         if (current > prev[index -1]) {
-            console.log(prev)
             prev.push(current)
+            return prev
          }  
         else {
-            console.log(prev)
             prev.splice(-1,0,current)
+            return sortedfunc(prev)
         }
-    return prev 
 },[])
 result.push(sortedfunc(random_list))
-
 
 result.map(e=>console.log(e))
